bc2c05c483e61eba009507052585ffcdccdd8ed9
Paulo Sales <paulounopar@hotmail.com>
2019-03-31 16:17:47 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/sergiosales/workshopmongo/config/Instantiation.java
+++ src/main/java/com/sergiosales/workshopmongo/config/Instantiation.java
@@ -1,30 +1,48 @@
 package com.sergiosales.workshopmongo.config;
 
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.sergiosales.workshopmongo.domain.Post;
 import com.sergiosales.workshopmongo.domain.User;
 import com.sergiosales.workshopmongo.repository.UserRepository;
+import com.sergiosales.workshopmongo.repository.PostRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PostRepository postRepository;
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 	
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		userRepository.saveAll(Arrays.asList(maria,alex,bob));
 		
+		
+		Post post1 = new Post(null, sdf.parse("21/03/18"),"Partiu viagem","Vou viajar para São Paulo. Abraços!",maria);
+		Post post2 = new Post(null, sdf.parse("21/03/18"),"Bom dia!","Acordei feliz hoje!",maria);
+
+	
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		postRepository.saveAll( Arrays.asList(post1,post2));
 	}
 
 }
--- src/main/java/com/sergiosales/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/sergiosales/workshopmongo/repository/PostRepository.java
@@ -2,10 +2,10 @@ package com.sergiosales.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import com.sergiosales.workshopmongo.domain.User;
 
+import com.sergiosales.workshopmongo.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 
 }

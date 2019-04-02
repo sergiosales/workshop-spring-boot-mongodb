52f9ba10a458f0cb510499700839d0000ef060a2
Paulo Sales <paulounopar@hotmail.com>
2019-04-02 20:14:35 -0300

    Consulta simples com @Query

--- src/main/java/com/sergiosales/workshopmongo/resources/PostResource.java
+++ src/main/java/com/sergiosales/workshopmongo/resources/PostResource.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sergiosales.workshopmongo.domain.Post;
 import com.sergiosales.workshopmongo.domain.User;
 import com.sergiosales.workshopmongo.dto.UserDTO;
+import com.sergiosales.workshopmongo.resources.util.URL;
 import com.sergiosales.workshopmongo.services.PostService;
 import com.sergiosales.workshopmongo.services.UserService;
 
@@ -35,6 +37,12 @@ public class PostResource {
 
 	}
 	
-	
+	@RequestMapping(value="/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value ="texte",defaultValue= "")String text) {
+		text = URL.decodeParam(text);
+		List<Post>list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+
+	}
 
 }
--- src/main/java/com/sergiosales/workshopmongo/services/PostService.java
+++ src/main/java/com/sergiosales/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.sergiosales.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,14 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
 	
+	public List<Post> findByTitle(String text){
+		
+		return post.findByTitleContainingIgnoreCase(text);
+	}
 	
 
 	
 	
 }
+
+
